918804d1f621dc74c7a7e2a1df81eb4b74a42cbb
ShopOne <oneky8080@gmail.com>
2020-02-14 21:51:48 +0900

    create damage_calc design

--- app/src/main/java/com/example/shirensupporter/MainActivity.kt
+++ app/src/main/java/com/example/shirensupporter/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.shirensupporter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        newPageBtn.setOnClickListener{
+            val intent = Intent(this@MainActivity,DamageCalc::class.java)
+            startActivity(intent)
+        }
     }
 }
