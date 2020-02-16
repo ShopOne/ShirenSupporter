4b832828f567fc61f091f53b0f4c309ee53f7ab9
ShopOne <oneky8080@gmail.com>
2020-02-16 18:56:01 +0900

    fix Gradle and layout

--- app/src/main/java/com/example/shirensupporter/ItemsFragment.kt
+++ app/src/main/java/com/example/shirensupporter/ItemsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.shirensupporter
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.item_table.*
 import kotlinx.android.synthetic.main.item_table.view.*
 import kotlinx.android.synthetic.main.table_row.view.*
+import org.w3c.dom.ls.LSResourceResolver
 
 
 class ItemsFragment(val cnt: Int): Fragment(){
@@ -19,14 +21,35 @@ class ItemsFragment(val cnt: Int): Fragment(){
     ): View? {
         val v =  inflater.inflate(R.layout.item_table,container,false)
         val table = v.tableLayout
-        var tableRow = inflater.inflate(R.layout.table_row,null)
+        var idx = 0
 
-        tableRow.nameRow.text = "ジェノサイド"
-        tableRow.itemBuyPrice.text = "ほげ"
-        tableRow.itemSellPrice.text = "ふが"
-        tableRow.itemFixedVal.text = "に"
+        for(item in items[0]){
+            val tableRow = inflater.inflate(R.layout.table_row,null)
+            tableRow.nameRow.text = item.first
+            tableRow.itemBuyPrice.text = item.second.toString()
+            tableRow.itemSellPrice.text = item.third.toString()
+            if(idx %2 == 0){
+                val col = resources.getColor(R.color.gray)
+                tableRow.nameRow.setBackgroundColor(col)
+                tableRow.itemBuyPrice.setBackgroundColor(col)
+                tableRow.itemSellPrice.setBackgroundColor(col)
+            }
+            idx++
 
-        table.addView(tableRow)
+            table.addView(tableRow)
+        }
+
         return v;
     }
+    val items = listOf(
+        listOf(
+            Triple("つるはし",240,100),
+            Triple("こん棒",240,80),
+            Triple("必中の剣",10000,5000),
+            Triple("妖刀かまいたち",5000,2000),
+            Triple("成仏の鎌",2000,900),
+            Triple("長巻",500,200),
+            Triple("ミノタウロスの斧",6000,2400)
+        )
+    )
 }
--- app/src/main/java/com/example/shirensupporter/UserDataBase.kt
+++ app/src/main/java/com/example/shirensupporter/UserDataBase.kt
@@ -3,7 +3,7 @@ package com.example.shirensupporter
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(MonsterEntity::class),version = 1)
+@Database(entities = arrayOf(MonsterEntity::class),version = 1,exportSchema = false)
 abstract class UserDataBase: RoomDatabase(){
     abstract fun userDao(): UserDao
 }
